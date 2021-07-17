a5e252a1c77d69c6a9f3f7678b0a715b09ca33d8
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 20:28:41 +0400

    chpass.py

--- chpass.py
+++ chpass.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python3
+#! -*- coding: utf-8 -*-
+print("Old password:")
+a = input()
+print("New password:")
+b = input()
+
 with open("/home/rootpass/pass.py", "r") as f:
-    raw = f.read().lower().replace("oldpass", "newpass")
+    raw = f.read().lower().replace(a, b)
     file = open("/home/rootpass/pass.py", "w")
     file.write(raw)
     file.close()
