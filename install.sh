fdb368a1a35d38a2c1493f9ed2514db3ab852ea5
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 12:12:43 +0400

    install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ cd /home
 mkdir rootpass
 sudo echo "alias sudo=\'python3 /home/rootpass/pass.py && sudo\'" >> /root/.bashrc
 sudo echo "alias sudo=\'python3 /home/rootpass/pass.py && sudo\'" >> /root/.zshrc
-mv ~/termuxloginpass/pass.py /home/rootpass
+sudo mv ~/termuxloginpass/pass.py /home/rootpass
 sudo chown root /home/rootpass/pass.py
 sudo chmod 700 /home/rootpass/pass.py
