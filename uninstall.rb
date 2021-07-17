d10f62542879c81a3baae8496bd40d55ce393865
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-17 12:13:36 +0400

    uninstall.rb

--- uninstall.rb
+++ uninstall.rb
@@ -1,2 +1,2 @@
-File.write("/root/.bashrc", File.open("/root/.bashrc",&:read).gsub("alias sudo=\'python3 ~/sudopass/pass.py && sudo\'", " "))
-File.write("/root/.zshrc", File.open("/root/.zshrc",&:read).gsub("alias sudo=\'python3 ~/sudopass/pass.py && sudo\'", " "))
+File.write("/root/.bashrc", File.open("/root/.bashrc",&:read).gsub("alias sudo=\'python3 /home/rootpass/pass.py && sudo\'", " "))
+File.write("/root/.zshrc", File.open("/root/.zshrc",&:read).gsub("alias sudo=\'python3 /home/rootpass/pass.py && sudo\'", " "))
